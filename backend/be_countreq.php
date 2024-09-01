9b3f545b387b0ebf9144d69abba9de1ce0aa896b
justjeng <jamesroldbaliscao@gmail.com>
2024-09-01 17:38:21 +0800

    maria db version error
    Error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'studentID = 666666' at line 1
    Co-Authored-By: Darwin Labiste <81823285+Darwinsuuu@users.noreply.github.com>

--- backend/be_countreq.php
+++ backend/be_countreq.php
@@ -1,15 +1,16 @@
 <?php
 try {
+    //session_start();
     include("_conn/connection.php");
 
     // Query to get total document requests
-    $totalQuery = "SELECT COUNT(*) AS total_requests FROM ezdrequesttbl";
+    $totalQuery = "SELECT COUNT(*) AS total_requests FROM ezdrequesttbl WHERE studentID = ".$_SESSION['studentId'];
     $totalResult = mysqli_query($conn, $totalQuery);
     $totalRow = mysqli_fetch_assoc($totalResult);
     $totalRequests = $totalRow['total_requests'];
 
     // Query to get pending document requests
-    $pendingQuery = "SELECT COUNT(*) AS pending_requests FROM ezdrequesttbl WHERE status = 'Pending'";
+    $pendingQuery = "SELECT COUNT(*) AS pending_requests FROM ezdrequesttbl WHERE status = 'Pending' studentID = ".$_SESSION['studentId'];
     $pendingResult = mysqli_query($conn, $pendingQuery);
     $pendingRow = mysqli_fetch_assoc($pendingResult);
     $pendingRequests = $pendingRow['pending_requests'];
